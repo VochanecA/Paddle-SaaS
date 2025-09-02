5f0596dc6df603c36199a7decaa79c1032832b6a
VochanecA <126856875+VochanecA@users.noreply.github.com>
2025-09-02 17:27:55 +0200

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 // tailwind.config.js
 module.exports = {
-  darkMode: "class", // 👈 important
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  zIndex: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        'background-alt': 'var(--background-alt)',
+        foreground: 'var(--foreground)',
+        accent: 'var(--accent)',
+        'accent-alt': 'var(--accent-alt)',
+      },
+      zIndex: {
         60: '60',
         70: '70',
         80: '80',
         90: '90',
         100: '100',
       },
+    },
   },
   plugins: [],
 };
